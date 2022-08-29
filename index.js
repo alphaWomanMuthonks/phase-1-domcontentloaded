b7fedb673f06586f53d071ef17ea97d9fc1fff6e
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-29 12:39:59 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+    return document.querySelector("#text").innerHTML=  "This is really cool!"
+});
+    // const event =document.createEvent('event');
+    // document.dispatchEvent(e)
+    // document.querySelector('p').textContent="This is really cool!" 
+
